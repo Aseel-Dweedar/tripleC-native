293a4a2871928706b0dd0d46158c75ef0901ddb6
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-12-08 23:34:56 +0200

    add authentication part 1

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,27 +1,67 @@
 "use strict";
 
+const bcrypt = require("bcrypt");
 const { userModel } = require("../models/user.model");
+const crypto = require("crypto");
 
-// // // // // // // CRETE // // // // // //
-const creatUser = (req, res) => {
-    let { name } = req.body;
-    userModel.findOne({ name: name }, (error, data) => {
-        if (error) {
-            res.send(error.message);
-        } else if (data) {
-            res.send("Already exist");
-        } else {
-            let newUser = new userModel({...req.body, request: [], car: [] });
-            newUser.save();
-            res.send(newUser);
-        }
-    });
+// // // // // // // global functions // // // //
+async function findByUsername(username) {
+  return await userModel.findOne({ username });
+}
+
+// // // // // // // CRETE // // // // // // //
+const creatUser = async (req, res) => {
+  try {
+    let { username, firstName, lastName, phone, password } = req.body;
+
+    let data = await findByUsername(username);
+
+    if (data) {
+      res.send("Username already exist");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      let newUser = new userModel({
+        username,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        phone,
+        image: "",
+        request: [],
+        car: [],
+      });
+      newUser.save();
+      res.send(newUser);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
 };
 
 // // // // // // // GET // // // // // //
-const getUser = async(req, res) => {
-    let data = await userModel.find({}).populate("cars requests");
-    res.send(data);
+const getUser = async (req, res) => {
+  let data = await userModel.find({}).populate("cars requests");
+  res.send(data);
 };
 
-module.exports = { creatUser, getUser };
+// // // // // // // Login // // // // // // //
+
+const userLogin = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    let user = await findByUsername(username);
+    if (!user) return res.send("Cannot find user !");
+    if (await bcrypt.compare(password, user.password)) {
+      const token = crypto.randomBytes(64).toString("hex");
+      console.log(token);
+      res.send(token);
+    } else {
+      res.send("Incorrect password !!");
+    }
+  } catch (error) {
+    return res.send(error.message);
+  }
+};
+
+module.exports = { creatUser, getUser, userLogin };
--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+
 const { creatService, getService, updateService } = require("./controllers/service.controller");
 const { creatRequest, getRequest, deleteRequest } = require("./controllers/request.controller");
-const { creatUser, getUser } = require("./controllers/user.controller");
+const { creatUser, getUser, userLogin } = require("./controllers/user.controller");
 const { creatCar, getCar, deleteCar } = require("./controllers/car.controller");
 const { creatLocation, getLocation, deleteLocation } = require("./controllers/location.controller");
+
 mongoose.connect("mongodb://localhost:27017/car-care", { useNewUrlParser: true });
 
 const app = express();
@@ -18,13 +20,20 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-    console.log("its /");
-    res.send("Hi From Axios");
+  res.send("Hi From Axios");
 });
 
 // // // // // // // User endpoint // // // // // //
 app.post("/user", creatUser);
 app.get("/user", getUser);
+app.post("/user/login", userLogin);
+
+// // // // // // // Authentication middleware // // // // // //
+// app.use((req, res, next) => {
+//   const authHeader = req.header["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
+//   if (token === null) res.sendStatus(401);
+// });
 
 // // // // // // // Service endpoint // // // // // //
 app.post("/service", creatService);
@@ -47,5 +56,5 @@ app.get("/location", getLocation);
 app.delete("/location/:locationId", deleteLocation);
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
+  console.log(`Server started on ${PORT}`);
 });
